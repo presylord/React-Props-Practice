adadc6aa24198320b74624251266e49a6c699ae6
Presy Lord Kidlat <91861180+presylord@users.noreply.github.com>
2021-11-30 15:12:46 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import Contacts from "./contacts";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
